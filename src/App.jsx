2bbf1151f7d302ccd3a0481486c78af7631353df
JavaOf <vistus707@gmail.com>
2025-11-11 14:07:12 +0600

    create clikcker

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./app.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    if (count < 30) setCount((prev) => prev + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) setCount((prev) => prev - 1);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="clicker">
+      <h2 className="name">Count: </h2>
+      <h1 className="count">{count}</h1>
+      <div className="btns-content">
+        <button className="increment" onClick={increment}>
+          +1
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button className="decrement" onClick={decrement}>
+          -1
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
